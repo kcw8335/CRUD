9c3274d6a9bcdc27c7ae5dbaa35fe3b12fcc96cd
changwookim <kcw8335@gmail.com>
2020-08-20 11:07:06 +0900

    hide secret key

--- CRUD_project/settings.py
+++ CRUD_project/settings.py
@@ -20,9 +20,25 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+# ==================================================================
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_u7jso6#!-i^$ka7mchs=k7ser6^!2f))mo3w_1efxzjnq6!##'
+import json
+from django.core.exceptions import ImproperlyConfigured
 
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
+#=====================================================================
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
